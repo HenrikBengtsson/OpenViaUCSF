ae71262b345df65e42e06d9b7c55f3bfc9d8c89d
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2012-07-12 14:07:08 -0700

    Adding a NEWS file:
    OpenViaUCSF v1.2.0 [2012-07-12]
    o If a page is already opened via the UCSF VPN, then it is possible
      to reopen it, or one of its links, without the UCSF VPN.

--- ChromeExtension/OpenViaUCSF/app.js
+++ ChromeExtension/OpenViaUCSF/app.js
@@ -2,79 +2,153 @@
 // Title: 'Open via USCF'
 // Author: Henrik Bengtsson
 // Created on: 2011-05-01
-// Last updated on: 2012-04-09
+// Last updated on: 2012-07-12
 // License: LGPL (>= 3)
 // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+function trimString(s) {
+  return s.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+}
 
-function addAddressBarIcon(tabId, changeInfo, tab) {
-  if (tab.url.indexOf("https://vpn.ucsf.edu/") != 0) {
+function isVpnUrl(url) {
+  url = trimString(url);
+  return (url.indexOf("https://vpn.ucsf.edu/") != -1);
+}
+
+function urlToVpnUrl(url) {
+  // Nothing to do?
+  if (isVpnUrl(url)) {
+    return url;
+  }
+  var vpnUrl = "https://vpn.ucsf.edu/dana/home/launch.cgi?url=";
+  url = trimString(url);
+  vpnUrl = vpnUrl + url;
+  return vpnUrl;
+}
+
+function vpnUrlToUrl(vpnUrl) {
+  // Nothing to do?
+  if (!isVpnUrl(vpnUrl)) {
+    return vpnUrl;
+  }
+
+  vpnUrl = trimString(vpnUrl);
+  var pattern = /https:[/][/]vpn.ucsf.edu[/](.*),DanaInfo=([^,]*)(|,(.*))[+](.*)/;
+  var mods = vpnUrl.replace(pattern, "$4");
+  // Known modifiers:
+  // "SSL": https
+  // "SSO=U": ?
+  var protocol = "http";
+  if (mods == "SSL") {
+    protocol = "https";
+  }
+  var url = vpnUrl.replace(pattern, protocol + "://$2/$1$5");
+  return url;
+}
+
+function updateAddressBarIcon(tabId, changeInfo, tab) {
+  if (isVpnUrl(tab.url)) {
+    chrome.pageAction.setTitle({tabId:tab.id, title:"Reopen current page without UCSF VPN"});
+    chrome.pageAction.show(tabId);
+  } else {
+    chrome.pageAction.setTitle({tabId:tab.id, title:"Reopen current page via UCSF VPN"});
     chrome.pageAction.show(tabId);
   }
 }
 
+function updateContextMenuEntries(tabId, changeInfo, tab) {
+  var json = localStorage["entries"];
+  
+  // Default entries
+  if (!json) {
+    json = '{ "USCF": { "name": "USCF VPN", "urlPrefix": "https://vpn.ucsf.edu/dana/home/launch.cgi?url=" } }'
+  }
+  
+  var contexts = ["page", "link"];
+  
+  var how = "via";
+  if (isVpnUrl(tab.url)) {
+    how = "without";
+  }
+
+  chrome.contextMenus.removeAll();
+  
+  var entries = JSON.parse(json);
+  for (var key in entries) {
+    var entry = entries[key];
+    var name = entry.name;
+    var urlPrefix = entry.urlPrefix;
+  
+    var title;
+  
+    for (var ii = 0; ii < contexts.length; ii++) {
+      var context = contexts[ii];
+  
+      if (context == "page") {
+        title = "Open current page in new tab " + how + " " + name;
+      } else if (context == "link") {
+        title = "Open link in new tab " + how + " " + name;
+      } else {
+        alert("Unknown context: " + context);
+      }
+  
+      var id = chrome.contextMenus.create({
+        "title": title, "contexts":[context], "onclick": onClick
+      });
+    } // for (var ii ...)
+  } // for (var key ...)
+}
+
+
 function onClickAddressBarIcon(tab) {
-  var url = "https://vpn.ucsf.edu/dana/home/launch.cgi?url=";
+  var url = null;
   if (tab.url) {
-    url = url + tab.url;
+    url = tab.url;
+  } else {
+    url = "";
+  }
+  if (isVpnUrl(url)) {
+    url = vpnUrlToUrl(url);
+  } else {
+    url = urlToVpnUrl(url);
   }
   chrome.tabs.update(null, {url: url});
 }
 
+function onTabUnpdate(tabId, changeInfo, tab) {
+  updateAddressBarIcon(tabId, changeInfo, tab);
+  updateContextMenuEntries(tabId, changeInfo, tab);
+}
+
 // Listen for any changes to the URL of any tab.
-chrome.tabs.onUpdated.addListener(addAddressBarIcon);
+chrome.tabs.onUpdated.addListener(onTabUnpdate);
 chrome.pageAction.onClicked.addListener(onClickAddressBarIcon);
 
 
 // The on-click callback function
 function onClick(info, tab) {
-  var url = "https://vpn.ucsf.edu/dana/home/launch.cgi?url=";
+  var url = null;
   if (info.linkUrl) {
-    url = url + info.linkUrl;
+    url = info.linkUrl;
   } else if (info.pageUrl) {
-    url = url + info.pageUrl;
+    url = info.pageUrl;
+  }
+  if (isVpnUrl(url)) {
+    url = vpnUrlToUrl(url);
+  } else {
+    url = urlToVpnUrl(url);
   }
   window.open(url);
 }
 
-var json = localStorage["entries"];
-
-// Default entries
-if (!json) {
-  json = '{ "USCF": { "name": "USCF VPN", "urlPrefix": "https://vpn.ucsf.edu/dana/home/launch.cgi?url=" } }'
-}
-
-var contexts = ["page", "link"];
-
-var entries = JSON.parse(json);
-for (var key in entries) {
-  var entry = entries[key];
-  var name = entry.name;
-  var urlPrefix = entry.urlPrefix;
-
-  var title;
-
-  for (var ii = 0; ii < contexts.length; ii++) {
-    var context = contexts[ii];
-
-    if (context == "page") {
-      title = "Open current page via " + name + " in new tab";
-    } else if (context == "link") {
-      title = "Open link via " + name + " in new tab";
-    } else {
-      alert("Unknown context: " + context);
-    }
-
-    var id = chrome.contextMenus.create({
-      "title": title, "contexts":[context], "onclick": onClick
-    });
-  } // for (var ii ...)
-} // for (var key ...)
-
-
-
 
 // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 // HISTORY:
+// 2012-07-12
+// o Now the context menu entries toggles between "Open ... via UCSF VPN"
+//   and "Open ... without UCSF VPN" depending on the current URL.  Same
+//   for the title of the address bar icon.
+// o Added updateContextMenuEntries(); no longer done in the main script.
+// o Added trimString(), isVpnUrl(), urlToVpnUrl() and vpnUrlToUrl().
 // 2012-04-09
 // o Added address bar icon.
 // o Made into a UCSF specific app.
